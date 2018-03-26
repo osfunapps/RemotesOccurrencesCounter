91cc6f4386e9cbfdadc48375287033429bee9a4c
osapps <osfunapps@gmail.com>
2018-03-26 19:06:59 +0300

    kol

--- src/RemoteOccurrencesCounter.py
+++ src/RemoteOccurrencesCounter.py
@@ -17,20 +17,33 @@ for filename in filesList:
 
 occurrencesDict = dict()
 
-for curr_name in names:
-    occurrencesDict[curr_name] = 1
+for name in names:
+    if(occurrencesDict.__contains__(name)):
+        occurrencesDict[name] +=1
+    else:
+        occurrencesDict[name] = 1
 
-for i in range(0, len(names) - 1):
-    for j in range(i + 1, len(names)):
-        if names[i] == names[j]:
-            ocCount = occurrencesDict[names[i]]
-            occurrencesDict[names[i]] = ocCount + 1
-
-for key, value in occurrencesDict.items():
-    print (key, value)
+#
+#
+#
+# for curr_name in names:
+#     occurrencesDict[curr_name] = 1
+#
+#
+#
+#
+# for i in range(0, len(names) - 1):
+#     for j in range(i + 1, len(names)):
+#         if names[i] == names[j]:
+#             ocCount = occurrencesDict[names[i]]
+#             occurrencesDict[names[i]] = ocCount + 1
+#
+# for key, value in occurrencesDict.items():
+#     print (key, value)
+#
 
 text_file = open("Remotes Occurrences.txt", "w")
-for key, value in occurrencesDict.iteritems():
-    text_file.write(str(key) + " " + str(value) + "\n")
+for key, value in sorted(occurrencesDict.iteritems(), key=lambda (k,v): (v,k), reverse=True):
+    text_file.write( "%s: %s" % (str(key), str(value))+"\n")
 
 text_file.close()
