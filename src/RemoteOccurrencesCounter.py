a532dd9e2a856336f6f7aed85a618a9782554721
osapps <osfunapps@gmail.com>
2018-03-26 18:39:36 +0300

    fsajk

---
+++ src/RemoteOccurrencesCounter.py
@@ -0,0 +1,36 @@
+import sys
+from os import walk
+
+path = sys.argv[1]
+filesList = []
+for (dirpath, dirnames, filenames) in walk(path):
+    filesList.extend(filenames)
+    break
+
+names = list()
+
+for filename in filesList:
+    idxStart = filename.find('_') + 1
+    idxEnd = len(filename) - filename[::-1].index('_') - 1
+    name = filename[idxStart:idxEnd]
+    names.append(name.lower())
+
+occurrencesDict = dict()
+
+for curr_name in names:
+    occurrencesDict[curr_name] = 1
+
+for i in range(0, len(names) - 1):
+    for j in range(i + 1, len(names)):
+        if names[i] == names[j]:
+            ocCount = occurrencesDict[names[i]]
+            occurrencesDict[names[i]] = ocCount + 1
+
+for key, value in occurrencesDict.iteritems():
+    print key, value
+
+text_file = open("Remotes Occurrences.txt", "w")
+for key, value in occurrencesDict.iteritems():
+    text_file.write(str(key) + " " + str(value) + "\n")
+
+text_file.close()
