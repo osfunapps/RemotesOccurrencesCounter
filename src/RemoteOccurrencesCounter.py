14a91f1ca3688ac5ecc5da9ea0e345eb4384a7f5
osapps <osfunapps@gmail.com>
2018-03-26 18:45:11 +0300

    lol

--- src/RemoteOccurrencesCounter.py
+++ src/RemoteOccurrencesCounter.py
@@ -27,7 +27,7 @@ for i in range(0, len(names) - 1):
             occurrencesDict[names[i]] = ocCount + 1
 
 for key, value in occurrencesDict.iteritems():
-    print key, value
+    print (key, value)
 
 text_file = open("Remotes Occurrences.txt", "w")
 for key, value in occurrencesDict.iteritems():
